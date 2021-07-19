046bdf6414b473d61aff2794525c7e9880878e1a
studentqa2021 <sarowerhome@gmail.com>
2021-07-19 08:42:51 -0400

    initial commit

---
+++ src/main/java/RAZIA/KHANAM.java
@@ -0,0 +1,40 @@
+package RAZIA;
+
+public class KHANAM {
+	
+void badal(){
+	
+}
+	
+int suma(){
+System.out.println("is the greatest");	return 9999;
+}
+	
+static int halim() {
+System.out.println("leader in the QA");	return 8888;
+}
+	
+void kazal() {
+	
+}
+	
+public static void main(String[] args) {
+System.out.println("Farhan");	
+	
+KHANAM object = new KHANAM();	
+object.suma();	
+	
+KHANAM.halim();	
+	
+	
+}	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
